0a07325312ede7a0e2fe8befba60948c966323e2
jmattock5 <80533767+jmattock5@users.noreply.github.com>
2022-10-06 12:37:50 +0100

    Delete extracting_fastas.sh

--- extracting_fastas.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kegg_list=$1
-fa=$2
-
-perl -ne 'if(/^>(\S+)/){$c=$i{$1}}$c?print:chomp;$i{$_}=1 if @ARGV' "$kegg_list" "$fa" #extracts the genes for each KEGG from the filtered prodigal output
-
